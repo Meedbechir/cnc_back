08a462d03b0187ecd56c99debc2e7f83ffe2d268
MeedBechir <medbechirathie@gmail.com>
2024-10-01 14:44:46 +0000

    First commit on this repo

---
+++ cnc_app/views.py
@@ -0,0 +1,45 @@
+from rest_framework import viewsets
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def create(self, request, *args, **kwargs):
+        designation = request.data.get('designation')
+        famille = request.data.get('famille')
+        origine = request.data.get('origine')
+        quantite = request.data.get('quantite')
+
+        if not designation or not famille or not origine or not quantite:
+            return Response({'error': 'Tous les champs doivent être remplis.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            quantite = int(quantite)
+            if quantite <= 0: 
+                return Response({'error': 'La quantité doit être un nombre entier positif.'}, status=status.HTTP_400_BAD_REQUEST)
+        except ValueError:
+            return Response({'error': 'La quantité doit être un nombre entier.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        articles = []
+        for i in range(quantite):
+            code_article = f"{designation[:3].lower()}{i+1}/{origine.lower()}"
+            article = Article(
+                designation=designation,
+                famille=famille,
+                origine=origine,
+                quantite=1,
+                code_article=code_article
+            )
+            articles.append(article)
+
+        Article.objects.bulk_create(articles)
+
+        created_articles = ArticleSerializer(articles, many=True).data
+
+        return Response({'message': f'{quantite} articles créés', 'articles': created_articles}, status=status.HTTP_201_CREATED)
---
+++ users/views.py
@@ -0,0 +1,34 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import SignupSerializer, LoginSerializer
+
+class SignupView(generics.CreateAPIView):
+    serializer_class = SignupSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()  # Save the user instance
+        return Response({"message": "Inscription réussie"}, status=201)
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                "message": "Connexion réussie"
+            }, status=200)
+        except Exception as e:
+            return Response({"non_field_errors": ["Email ou mot de passe invalide"]}, status=400)
